f6d1c306afefced55e455c98209c531e982895b0
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-02 14:10:14 -0700

    added placeholder footer and header

--- InformationalPage/src/App.jsx
+++ InformationalPage/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header siteTitle="Animal Emergency Guide"/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer siteTitle="Animal Emergency Guide"/>
     </>
   )
 }
