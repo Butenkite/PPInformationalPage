dba488a1e8fcc5caed261012706a9b859a3331e0
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-02 14:35:35 -0700

    need image

--- InformationalPage/src/App.jsx
+++ InformationalPage/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { CardList } from "./CardList"
+import { parseReviews } from "./reviewsParser";
+import rawReviews from "./assets/review.json";
+
+const reviews = parseReviews(rawReviews);
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <Header siteTitle="Animal Emergency Guide"/>
-      <div>
+        <main>
+          <h1>Game Reviews</h1>
+          <CardList reviews={reviews} />
+        </main>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,7 +40,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      
+
       <Footer siteTitle="Animal Emergency Guide"/>
     </>
   )
